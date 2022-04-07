825d373d6d291957cc49f1e8352fe3872dd7d6aa
Daniel Hlazunow <tresdio1@gmail.com>
2022-04-07 14:49:53 +0300

    first

--- app.ts
+++ app.ts
@@ -2,7 +2,3 @@ const com = 'simpe commit';
 const third = '3323223';
 const com1 = 'fsdibfj';
 const com2am = 'fsdibfj';
-const com3 = 'fsdibfj';
-const com4am = 'fsdibfj';
-const com5 = 'fsdibfj';
-const com6am = 'fsdibfj';
